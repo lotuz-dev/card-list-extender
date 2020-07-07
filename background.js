aee8e7dda99ee60bb5e8cca6f700fa2c7f44f1e1
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-07-07 17:18:41 -0300

    Background.js injects inject.js only on allowed domains

--- background.js
+++ background.js
@@ -1,46 +1,21 @@
-window.onload = () => {
-  setTimeout(() => {
+const EXPAND = "↔️";
+const ALLOWED_DOMAINS = [
+  "https://gitlab.com/",
+  "https://github.com/",
+  "https://trello.com/",
+];
 
-    let isFullScreen = false;
-    let countBadges = document.getElementsByClassName("board-list");
+chrome.tabs.onUpdated.addListener(function callback(id, info, tab) {
+  if (tab.status !== "loading") {
+    return;
+  }
 
-    countBadges.each((i, badge) => {
-      
-      let expander = $(`
-<button aria-label="Expand list" data-placement="bottom" title="" type="button" class="border-0 p-0 board-delete board-expand has-tooltip float-right" data-original-title="Expand list">
-<i aria-hidden="true" data-hidden="true" class="fa fa-expand"></i>
-</button>
-`);
+  if (!ALLOWED_DOMAINS.some((url) => tab.url.startsWith(url))) {
+    return;
+  }
 
-      expander.insertBefore(badge);
-
-      expander.click(function () {
-        
-        let board = boardInner.parent();
-        let ul = document.getElementsByTagName("ul", boardInner);
-
-        if (isFullScreen) {
-          // document.exitFullscreen()
-          ul.css("display", "block");
-          board.css("width", "");
-        } else {
-          // board[0].requestFullscreen()
-          board.css("width", "100%");
-          ul.css("display", "grid");
-          ul.css("grid-template-columns", "1fr 1fr 1fr 1fr 1fr");
-          ul.css("grid-auto-rows", "25%");
-
-          ul[0].scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-            inline: "center",
-          });
-        }
-
-        $("i", this).toggleClass("fa-expand fa-compress");
-
-        isFullScreen = !isFullScreen;
-      });
-    });
-  }, 1000);
-};
+  // TODO: Ensure script is injected only once
+  chrome.tabs.executeScript(tab.id, {
+    file: "./inject.js",
+  });
+});
