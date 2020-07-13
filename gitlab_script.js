456582cafa7415aa0587e1dd1f604aeb7ded587a
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-07-13 10:07:22 -0300

    Add a retry strategy to inject buttons

--- gitlab_script.js
+++ gitlab_script.js
@@ -1,11 +1,6 @@
-window.onload = () => {
-  const EXPAND_SVG = `
-      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="expand-alt" class="svg-inline--fa fa-expand-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M212.686 315.314L120 408l32.922 31.029c15.12 15.12 4.412 40.971-16.97 40.971h-112C10.697 480 0 469.255 0 456V344c0-21.382 25.803-32.09 40.922-16.971L72 360l92.686-92.686c6.248-6.248 16.379-6.248 22.627 0l25.373 25.373c6.249 6.248 6.249 16.378 0 22.627zm22.628-118.628L328 104l-32.922-31.029C279.958 57.851 290.666 32 312.048 32h112C437.303 32 448 42.745 448 56v112c0 21.382-25.803 32.09-40.922 16.971L376 152l-92.686 92.686c-6.248 6.248-16.379 6.248-22.627 0l-25.373-25.373c-6.249-6.248-6.249-16.378 0-22.627z"></path></svg>
-    `;
-
-  const COMPRESS_SVG = `
-      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="compress-alt" class="svg-inline--fa fa-compress-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M4.686 427.314L104 328l-32.922-31.029C55.958 281.851 66.666 256 88.048 256h112C213.303 256 224 266.745 224 280v112c0 21.382-25.803 32.09-40.922 16.971L152 376l-99.314 99.314c-6.248 6.248-16.379 6.248-22.627 0L4.686 449.941c-6.248-6.248-6.248-16.379 0-22.627zM443.314 84.686L344 184l32.922 31.029c15.12 15.12 4.412 40.971-16.97 40.971h-112C234.697 256 224 245.255 224 232V120c0-21.382 25.803-32.09 40.922-16.971L296 136l99.314-99.314c6.248-6.248 16.379-6.248 22.627 0l25.373 25.373c6.248 6.248 6.248 16.379 0 22.627z"></path></svg>
-    `;
+window.onload = async () => {
+  const $ = queryOne;
+  const $$ = queryAll;
 
   const strategies = {
     gitlab: {
@@ -32,15 +27,12 @@ window.onload = () => {
     },
   };
 
-
   let strategy = strategies.gitlab;
 
-  function $(seletor, element = document) {
-    return element.querySelector(seletor);
-  }
+  let isReady = await linearBackoff(() => $$(strategy.anchorSelector).length);
 
-  function $$(seletor, element = document) {
-    return element.querySelectorAll(seletor);
+  if (!isReady) {
+    return;
   }
 
   let anchors = $$(strategy.anchorSelector);
--- trello_script.js
+++ trello_script.js
@@ -1,11 +1,6 @@
 window.onload = () => {
-  const EXPAND_SVG = `
-    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="expand-alt" class="svg-inline--fa fa-expand-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M212.686 315.314L120 408l32.922 31.029c15.12 15.12 4.412 40.971-16.97 40.971h-112C10.697 480 0 469.255 0 456V344c0-21.382 25.803-32.09 40.922-16.971L72 360l92.686-92.686c6.248-6.248 16.379-6.248 22.627 0l25.373 25.373c6.249 6.248 6.249 16.378 0 22.627zm22.628-118.628L328 104l-32.922-31.029C279.958 57.851 290.666 32 312.048 32h112C437.303 32 448 42.745 448 56v112c0 21.382-25.803 32.09-40.922 16.971L376 152l-92.686 92.686c-6.248 6.248-16.379 6.248-22.627 0l-25.373-25.373c-6.249-6.248-6.249-16.378 0-22.627z"></path></svg>
-  `;
-
-  const COMPRESS_SVG = `
-    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="compress-alt" class="svg-inline--fa fa-compress-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M4.686 427.314L104 328l-32.922-31.029C55.958 281.851 66.666 256 88.048 256h112C213.303 256 224 266.745 224 280v112c0 21.382-25.803 32.09-40.922 16.971L152 376l-99.314 99.314c-6.248 6.248-16.379 6.248-22.627 0L4.686 449.941c-6.248-6.248-6.248-16.379 0-22.627zM443.314 84.686L344 184l32.922 31.029c15.12 15.12 4.412 40.971-16.97 40.971h-112C234.697 256 224 245.255 224 232V120c0-21.382 25.803-32.09 40.922-16.971L296 136l99.314-99.314c6.248-6.248 16.379-6.248 22.627 0l25.373 25.373c6.248 6.248 6.248 16.379 0 22.627z"></path></svg>
-  `;
+  const $ = queryOne;
+  const $$ = queryAll;
 
   const strategies = {
     trello: {
@@ -34,15 +29,13 @@ window.onload = () => {
     },
   };
 
-  function $(seletor, element = document) {
-    return element.querySelector(seletor);
-  }
-
-  function $$(seletor, element = document) {
-    return element.querySelectorAll(seletor);
-  }
-
   let strategy = strategies.trello;
+
+  let isReady = await linearBackoff(() => $$(strategy.anchorSelector).length);
+
+  if (!isReady) {
+    return;
+  }
 
   let anchors = $$(strategy.anchorSelector);
 
@@ -64,19 +57,20 @@ window.onload = () => {
 
         board.setAttribute("style", "");
 
-        board.style.display="inline-block";
+        board.style.display = "inline-block";
         cardList.style.display = "block";
         icon.innerHTML = EXPAND_SVG;
       } else {
-
         this.setAttribute("title", "Compress list");
 
         board.setAttribute("style", `width: 98vw;`);
-        cardList.scrollIntoView({
+        setTimeout(() => {
+          board.scrollIntoView({
             behavior: "smooth",
             block: "start",
-            inline:'center'
+            inline: "center",
           });
+        }, 100);
         cardList.style.display = "grid";
 
         cardList.style[
@@ -87,7 +81,6 @@ window.onload = () => {
 
         cardList.style["grid-gap"] = "4px";
 
-       
         icon.innerHTML = COMPRESS_SVG;
       }
 
